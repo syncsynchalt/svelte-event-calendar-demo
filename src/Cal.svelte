7062f343df01ce3a59c59b6db2bb65ede738ebea
Michael Driscoll <michael@xargs.org>
2024-10-12 12:03:09 -0600

    Convert to TS

--- src/Cal.svelte
+++ src/Cal.svelte
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
     import Calendar from "@event-calendar/core";
     import TimeGrid from "@event-calendar/time-grid";
 
     let plugins = [TimeGrid];
-    let options = {
+    let options: Calendar.Options = {
         view: "timeGridWeek",
     };
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import Cal from "../Cal.svelte";
 </script>
 
