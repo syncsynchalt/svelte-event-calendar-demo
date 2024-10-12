2a893723f168f9bd3ae71632848b5cb834c5caf2
Michael Driscoll <michael@xargs.org>
2024-10-12 12:09:39 -0600

    Minimal repro

--- src/Cal.svelte
+++ src/Cal.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
     import Calendar from "@event-calendar/core";
     import TimeGrid from "@event-calendar/time-grid";
-
-    let plugins = [TimeGrid];
-    let options: Calendar.Options = {
-        view: "timeGridWeek",
-    };
-
-    function updateOptions() {
-        options.slotDuration = "01:00";
-    }
 </script>
 
-<button on:click={updateOptions}>Change slot duration</button>
-<Calendar {plugins} {options} />
+<Calendar plugins={[TimeGrid]} options={{}}/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     import Cal from "../Cal.svelte";
 </script>
 
-<h1>Welcome to my calendar demo</h1>
+<h1>Calendar demo</h1>
 
 <Cal />
