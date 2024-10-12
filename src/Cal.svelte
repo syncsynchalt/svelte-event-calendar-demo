37953cee0373d6483f3d6bd1a51cbc6265b62182
Michael Driscoll <michael@xargs.org>
2024-10-12 11:56:51 -0600

    Basic calendar demo

---
+++ src/Cal.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek'
+    };
+
+    function updateOptions() {
+        options.slotDuration = '01:00';
+    }
+</script>
+
+<button on:click={updateOptions}>Change slot duration</button>
+<Calendar {plugins} {options} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Cal from "../Cal.svelte";
+</script>
+
+<h1>Welcome to my calendar demo</h1>
+
+<Cal />
